487ebcec15326dd162eb8c22b8b53a0e31d9d06d
fabioyk <kyongxd@gmail.com>
2018-04-19 18:07:00 -0300

    Auto-generated commit

---
+++ main.bundle.js
@@ -0,0 +1,353 @@
+webpackJsonp([1,4],{
+
+/***/ 137:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(22)();
+// imports
+
+
+// module
+exports.push([module.i, "app-prize {\r\n  cursor: pointer;\r\n}\r\n\r\n.prize-pack {\r\n  width: 300px;\r\n  border: 1px solid rgba(255, 255, 255, 0.3);\r\n  border-radius: 10px;\r\n  padding: 10px;\r\n}\r\n\r\n.next-indicator {\r\n  cursor: pointer;\r\n  float:left;\r\n  position: absolute;\r\n  transition-property: left;\r\n  transition-duration: 0.2s;\r\n}\r\n\r\napp-enemy-list {\r\n  cursor: pointer;  \r\n}\r\n\r\n.no-select {\r\n  -webkit-touch-callout: none; /* iOS Safari */\r\n  -webkit-user-select: none; /* Safari */ /* Konqueror HTML */\r\n     -moz-user-select: none; /* Firefox */\r\n      -ms-user-select: none; /* Internet Explorer/Edge */\r\n          user-select: none; /* Non-prefixed version, currently\r\n                                supported by Chrome and Opera */\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 138:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(22)();
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 139:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(22)();
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 140:
+/***/ (function(module, exports) {
+
+module.exports = "<h2 class='no-select'>\n  NMG Prize Pack Tracker\n</h2>\n\n<div class='prize-pack no-select'>\n  <img class='next-indicator no-select' src='assets/next.png'\n    [ngStyle]=\"{'left': getPackCoords(5)}\" (click)='onAdvancePrize(5)'>\n  <div class='prize-row no-select'>\n    <app-prize *ngFor='let item of pack5; let i = index'\n      itemName={{item}} (click)='onClickPrize(5, i)'></app-prize>\n  </div>\n  <app-enemy-list class='no-select' prizePackNumber=5 (click)='onAdvancePrize(5)'>\n  </app-enemy-list>\n</div>\n<br>\n<div class='prize-pack no-select'>\n  <img class='next-indicator' src='assets/next.png'\n    [ngStyle]=\"{'left': getPackCoords(6)}\" (click)='onAdvancePrize(6)'>\n  <div class='prize-row'>\n      <app-prize *ngFor='let item of pack6; let i = index'\n      itemName={{item}} (click)='onClickPrize(6, i)'></app-prize>\n  </div>\n  <app-enemy-list class='no-select' prizePackNumber=6 (click)='onAdvancePrize(6)'>\n  </app-enemy-list>\n</div>"
+
+/***/ }),
+
+/***/ 141:
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  <img [src]='enemyImageUrl'>\n</p>\n"
+
+/***/ }),
+
+/***/ 142:
+/***/ (function(module, exports) {
+
+module.exports = "<img [src]='itemImageUrl'>\n"
+
+/***/ }),
+
+/***/ 172:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(73);
+
+
+/***/ }),
+
+/***/ 72:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 72;
+
+
+/***/ }),
+
+/***/ 73:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(78);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(80);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(83);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(4);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.pack5 = ['smallMagic', 'rupee', 'heart', '5arrows', 'smallMagic', 'bomb', 'rupee', 'heart'];
+        this.pack6 = ['heart', 'fairy', 'fullMagic', 'redRupee', '8bomb', 'heart', 'redRupee', '10arrows'];
+        this.current5 = 0;
+        this.current6 = 0;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+    };
+    AppComponent.prototype.onClickPrize = function (pack, index) {
+        if (pack === 5) {
+            if (this.current5 === index) {
+                this.current5++;
+            }
+            else {
+                this.current5 = index;
+            }
+        }
+        else {
+            if (this.current6 === index) {
+                this.current6++;
+            }
+            else {
+                this.current6 = index;
+            }
+        }
+        if (this.current5 > 7) {
+            this.current5 = 0;
+        }
+        if (this.current6 > 7) {
+            this.current6 = 0;
+        }
+    };
+    AppComponent.prototype.onAdvancePrize = function (pack) {
+        if (pack === 5) {
+            this.current5++;
+        }
+        else {
+            this.current6++;
+        }
+        if (this.current5 > 7) {
+            this.current5 = 0;
+        }
+        if (this.current6 > 7) {
+            this.current6 = 0;
+        }
+    };
+    AppComponent.prototype.getPackCoords = function (packN) {
+        var px = 15;
+        if (packN === 5) {
+            px += this.current5 * 36;
+        }
+        else {
+            px += this.current6 * 36;
+        }
+        return px + 'px';
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(140),
+        styles: [__webpack_require__(137)]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 80:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(30);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(77);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__(79);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__prize_prize_component__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__enemy_list_enemy_list_component__ = __webpack_require__(81);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_4__prize_prize_component__["a" /* PrizeComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__enemy_list_enemy_list_component__["a" /* EnemyListComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 81:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(4);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EnemyListComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var EnemyListComponent = (function () {
+    function EnemyListComponent() {
+    }
+    EnemyListComponent.prototype.ngOnInit = function () {
+        if (this.prizePackNumber) {
+            this.enemyImageUrl = 'assets/pack' + this.prizePackNumber + '.png';
+        }
+    };
+    return EnemyListComponent;
+}());
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Input */])(),
+    __metadata("design:type", Number)
+], EnemyListComponent.prototype, "prizePackNumber", void 0);
+EnemyListComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-enemy-list',
+        template: __webpack_require__(141),
+        styles: [__webpack_require__(138)]
+    }),
+    __metadata("design:paramtypes", [])
+], EnemyListComponent);
+
+//# sourceMappingURL=enemy-list.component.js.map
+
+/***/ }),
+
+/***/ 82:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(4);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PrizeComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var PrizeComponent = (function () {
+    function PrizeComponent() {
+    }
+    PrizeComponent.prototype.ngOnInit = function () {
+        if (this.itemName) {
+            this.itemImageUrl = 'assets/drops/' + this.itemName + '.png';
+        }
+    };
+    return PrizeComponent;
+}());
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Input */])(),
+    __metadata("design:type", String)
+], PrizeComponent.prototype, "itemName", void 0);
+PrizeComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-prize',
+        template: __webpack_require__(142),
+        styles: [__webpack_require__(139)]
+    }),
+    __metadata("design:paramtypes", [])
+], PrizeComponent);
+
+//# sourceMappingURL=prize.component.js.map
+
+/***/ }),
+
+/***/ 83:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+var environment = {
+    production: true
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[172]);
+//# sourceMappingURL=main.bundle.js.map
